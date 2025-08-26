356518ec0a2b7e9f84d11eb47c89e5a5ef8cdc44
Chandler Ferry <chandlerferry@gmail.com>
2025-08-26 01:47:17 -0700

    fix: better optional type

--- src/types/sox/api/bot/index.d.ts
+++ src/types/sox/api/bot/index.d.ts
@@ -26,103 +26,91 @@
 /// <reference path="./widgets.d.ts" />
 
 declare namespace bot {
-    // Core methods are accessed directly on the bot object
-    interface SoxBotApi extends bot {
+	// Core methods are accessed directly on the bot object
+	interface SoxBotApi extends bot {
+		// Namespaces
+		attackStyle: bot.attackStyle;
+		bank: bot.bank;
+		bmCache: bot.bmCache;
+		bmGlobalCache: bot.bmGlobalCache;
+		breakHandler: bot.breakHandler;
+		counters: bot.counters;
+		equipment: bot.equipment;
+		events: bot.events;
+		grandExchange: bot.grandExchange;
+		graphicsObjects: bot.graphicsObjects;
+		inventory: bot.inventory;
+		magic: bot.magic;
+		net: bot.net;
+		notifier: bot.notifier;
+		npcs: bot.npcs;
+		objects: bot.objects;
+		players: bot.players;
+		plugins: bot.plugins;
+		prayer: bot.prayer;
+		projectiles: bot.projectiles;
+		task: bot.task;
+		tileItems: bot.tileItems;
+		variables: bot.variables;
+		walking: bot.walking;
+		widgets: bot.widgets;
 
-        // Namespaces
-        attackStyle: bot.attackStyle;
-        bank: bot.bank;
-        bmCache: bot.bmCache;
-        bmGlobalCache: bot.bmGlobalCache;
-        breakHandler: bot.breakHandler;
-        counters: bot.counters;
-        equipment: bot.equipment;
-        events: bot.events;
-        grandExchange: bot.grandExchange;
-        graphicsObjects: bot.graphicsObjects;
-        inventory: bot.inventory;
-        magic: bot.magic;
-        net: bot.net;
-        notifier: bot.notifier;
-        npcs: bot.npcs;
-        objects: bot.objects;
-        players: bot.players;
-        plugins: bot.plugins;
-        prayer: bot.prayer;
-        projectiles: bot.projectiles;
-        task: bot.task;
-        tileItems: bot.tileItems;
-        variables: bot.variables;
-        walking: bot.walking;
-        widgets: bot.widgets;
+		// Direct methods
+		/**
+		 * Performs a menu action.
+		 * @param p0 The first parameter for the menu action
+		 * @param p1 The second parameter for the menu action
+		 * @param action The menu action to perform
+		 * @param identifier The identifier for the menu action
+		 * @param itemId The item ID for the menu action
+		 * @param option The option for the menu action
+		 * @param target The target for the menu action
+		 * @param bounds The bounds for the menu action
+		 */
+		menuAction(
+			p0: number,
+			p1: number,
+			action: MenuAction,
+			identifier: number,
+			itemId: number,
+			option: string,
+			target: string,
+			bounds?: Rectangle,
+		): void;
+		/**
+		 * Clears the game chat.
+		 */
+		clearGameChat: () => void;
+		/**
+		 * Checks if the local player is idle.
+		 * @returns True if the local player is idle, false otherwise.
+		 */
+		localPlayerIdle: () => boolean;
+		/**
+		 * Checks if the local player is moving.
+		 * @returns True if the local player is moving, false otherwise.
+		 */
+		localPlayerMoving: () => boolean;
+		/**
+		 * Prints a message to the game chat.
+		 * @param message The message to print.
+		 */
+		printGameMessage: (message: string) => void;
+		/**
+		 * Prints a message to the log.
+		 * @param message The message to print.
+		 */
+		printLogMessage: (message: string) => void;
 
-        // Direct methods
-        /**
-         * Performs a menu action.
-         * @param p0 The first parameter for the menu action
-         * @param p1 The second parameter for the menu action
-         * @param action The menu action to perform
-         * @param identifier The identifier for the menu action
-         * @param itemId The item ID for the menu action
-         * @param option The option for the menu action
-         * @param target The target for the menu action
-         * @param bounds The bounds for the menu action
-         */
-        menuAction: {
-            (
-                p0: number,
-                p1: number,
-                action: net.runelite.api.MenuAction,
-                identifier: number,
-                itemId: number,
-                option: string,
-                target: string
-            ): void;
-            (
-                p0: number,
-                p1: number,
-                action: net.runelite.api.MenuAction,
-                identifier: number,
-                itemId: number,
-                option: string,
-                target: string,
-                bounds: java.awt.Rectangle
-            ): void;
-        };
-        /**
-         * Clears the game chat.
-         */
-        clearGameChat: () => void;
-        /**
-         * Checks if the local player is idle.
-         * @returns True if the local player is idle, false otherwise.
-         */
-        localPlayerIdle: () => boolean;
-        /**
-         * Checks if the local player is moving.
-         * @returns True if the local player is moving, false otherwise.
-         */
-        localPlayerMoving: () => boolean;
-        /**
-         * Prints a message to the game chat.
-         * @param message The message to print.
-         */
-        printGameMessage: (message: string) => void;
-        /**
-         * Prints a message to the log.
-         * @param message The message to print.
-         */
-        printLogMessage: (message: string) => void;
+		/**
+		 * Runs a client script.
+		 * @param ints The integer parameters for the script.
+		 */
+		runClientScript: (ints: number[]) => void;
 
-        /**
-         * Runs a client script.
-         * @param ints The integer parameters for the script.
-         */
-        runClientScript: (ints: number[]) => void;
-
-        /**
-         * Terminates the bot.
-         */
-        terminate: () => void;
-    }
+		/**
+		 * Terminates the bot.
+		 */
+		terminate: () => void;
+	}
 }
