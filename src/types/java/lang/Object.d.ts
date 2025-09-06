0e3113078d2af9af018f0ce58e5d3b0c2d7aed2e
Chandler Ferry <chandlerferry@gmail.com>
2025-09-05 22:11:14 -0700

    chore: add reflection types

--- src/types/java/lang/Object.d.ts
+++ src/types/java/lang/Object.d.ts
@@ -5,15 +5,15 @@ declare namespace java {
 			equals(obj: any): boolean;
 			hashCode(): number;
 			toString(): string;
-			getClass(): java.lang.Class;
+			getClass(): java.lang.Class<any>;
 			notify(): void;
 			notifyAll(): void;
 			wait(timeout?: number, nanos?: number): void;
 		}
 
-		export class Class extends Object {
+		export class Class<T = any> extends Object {
 			asSubclass<U>(clazz: Class<U>): Class<U>;
-			cast<T>(obj: any): T;
+			cast<U>(obj: any): U;
 			desiredAssertionStatus(): boolean;
 			static forName(className: string): Class<any>;
 			static forName(name: string, initialize: boolean, loader: any): Class<any>;
@@ -26,30 +26,30 @@ declare namespace java {
 			getClasses(): Class<any>[];
 			getClassLoader(): any;
 			getComponentType(): Class<any>;
-			getConstructor<T>(...parameterTypes: Class<any>[]): any;
+			getConstructor<U>(...parameterTypes: Class<any>[]): any;
 			getConstructors(): any[];
 			getDeclaredAnnotation<A extends any>(annotationClass: Class<A>): A | null;
 			getDeclaredAnnotations(): any[];
 			getDeclaredAnnotationsByType<A extends any>(annotationClass: Class<A>): A[];
 			getDeclaredClasses(): Class<any>[];
-			getDeclaredConstructor<T>(...parameterTypes: Class<any>[]): any;
+			getDeclaredConstructor<U>(...parameterTypes: Class<any>[]): any;
 			getDeclaredConstructors(): any[];
-			getDeclaredField(name: string): any;
-			getDeclaredFields(): any[];
-			getDeclaredMethod(name: string, ...parameterTypes: Class<any>[]): any;
-			getDeclaredMethods(): any[];
+			getDeclaredField(name: string): java.lang.reflect.Field;
+			getDeclaredFields(): java.lang.reflect.Field[];
+			getDeclaredMethod(name: string, ...parameterTypes: Class<any>[]): java.lang.reflect.Method;
+			getDeclaredMethods(): java.lang.reflect.Method[];
 			getDeclaringClass(): Class<any>;
 			getEnclosingClass(): Class<any>;
 			getEnclosingConstructor(): any;
-			getEnclosingMethod(): any;
-			getEnumConstants<T>(): T[] | null;
-			getField(name: string): any;
-			getFields(): any[];
+			getEnclosingMethod(): java.lang.reflect.Method;
+			getEnumConstants<U>(): U[] | null;
+			getField(name: string): java.lang.reflect.Field;
+			getFields(): java.lang.reflect.Field[];
 			getGenericInterfaces(): any[];
 			getGenericSuperclass(): any;
 			getInterfaces(): Class<any>[];
-			getMethod(name: string, ...parameterTypes: Class<any>[]): any;
-			getMethods(): any[];
+			getMethod(name: string, ...parameterTypes: Class<any>[]): java.lang.reflect.Method;
+			getMethods(): java.lang.reflect.Method[];
 			getModifiers(): number;
 			getName(): string;
 			getPackage(): any;
@@ -58,7 +58,7 @@ declare namespace java {
 			getResourceAsStream(name: string): any;
 			getSigners(): any[];
 			getSimpleName(): string;
-			getSuperclass<T>(): Class<T>;
+			getSuperclass<U>(): Class<U>;
 			getTypeName(): string;
 			getTypeParameters(): any[];
 			isAnnotation(): boolean;
@@ -73,9 +73,65 @@ declare namespace java {
 			isMemberClass(): boolean;
 			isPrimitive(): boolean;
 			isSynthetic(): boolean;
-			newInstance<T>(): T;
+			newInstance<U>(): U;
 			toGenericString(): string;
 			toString(): string;
+		}
+	}
+
+	export namespace reflect {
+		export class Field extends java.lang.Object {
+			get(obj: any): any;
+			getBoolean(obj: any): boolean;
+			getByte(obj: any): number;
+			getChar(obj: any): string;
+			getDouble(obj: any): number;
+			getFloat(obj: any): number;
+			getInt(obj: any): number;
+			getLong(obj: any): number;
+			getShort(obj: any): number;
+			set(obj: any, value: any): void;
+			setBoolean(obj: any, value: boolean): void;
+			setByte(obj: any, value: number): void;
+			setChar(obj: any, value: string): void;
+			setDouble(obj: any, value: number): void;
+			setFloat(obj: any, value: number): void;
+			setInt(obj: any, value: number): void;
+			setLong(obj: any, value: number): void;
+			setShort(obj: any, value: number): void;
+			getDeclaringClass(): java.lang.Class<any>;
+			getName(): string;
+			getType(): java.lang.Class<any>;
+			getModifiers(): number;
+			isEnumConstant(): boolean;
+			isSynthetic(): boolean;
+			toString(): string;
+		}
+
+		export class Method extends java.lang.Object {
+			invoke(obj: any, ...args: any[]): any;
+			getDeclaringClass(): java.lang.Class<any>;
+			getName(): string;
+			getParameterTypes(): java.lang.Class<any>[];
+			getReturnType(): java.lang.Class<any>;
+			getModifiers(): number;
+			getParameterCount(): number;
+			isAccessible(): boolean;
+			setAccessible(flag: boolean): void;
+			isVarArgs(): boolean;
+			isSynthetic(): boolean;
+			isBridge(): boolean;
+			isDefault(): boolean;
+			getGenericParameterTypes(): any[];
+			getGenericReturnType(): any;
+			getGenericExceptionTypes(): any[];
+			getExceptionTypes(): java.lang.Class<any>[];
+			getAnnotation<A extends any>(annotationClass: java.lang.Class<A>): A | null;
+			getAnnotations(): any[];
+			getDeclaredAnnotations(): any[];
+			getParameterAnnotations(): any[][];
+			toString(): string;
+			toGenericString(): string;
 		}
 	}
 }
