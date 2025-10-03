69e6400423d13c4185b35a632469c98e3c160a95
ChandlerFerry <chandlerferry@gmail.com>
2025-10-03 14:59:38 -0700

    chore: runelite menu

---
+++ src/types/runelite/net/runelite/api/Menu.d.ts
@@ -0,0 +1,65 @@
+/// <reference path="../../../../../../src/types/runelite/index.d.ts" />
+declare namespace net.runelite.api {
+  /**
+   * The client minimenu.
+   */
+  export interface Menu {
+    /**
+     * Create a new menu entry
+     * @param idx the index to create the menu entry at. Accepts negative indexes eg. -1 inserts at the end.
+     * @return the newly created menu entry
+     */
+    createMenuEntry(idx: number): MenuEntry;
+
+    /**
+     * Gets the current mini menu entries.
+     *
+     * @return array of menu entries
+     */
+    getMenuEntries(): MenuEntry[];
+
+    /**
+     * Sets the array of menu entries.
+     *
+     * This method should typically be used in the context of the MenuOpened
+     * event, since setting the menu entries will be overwritten the next frame
+     *
+     * @param entries new array of open menu entries
+     */
+    setMenuEntries(entries: MenuEntry[]): void;
+
+    /**
+     * Remove a menu entry
+     * @param entry the menu entry
+     */
+    removeMenuEntry(entry: MenuEntry): void;
+
+    /**
+     * Get the menu x location. Only valid if the menu is open.
+     *
+     * @return the menu x location
+     */
+    getMenuX(): number;
+
+    /**
+     * Get the menu y location. Only valid if the menu is open.
+     *
+     * @return the menu y location
+     */
+    getMenuY(): number;
+
+    /**
+     * Get the menu width. Only valid if the menu is open.
+     *
+     * @return the menu width
+     */
+    getMenuWidth(): number;
+
+    /**
+     * Get the menu height. Only valid if the menu is open.
+     *
+     * @return the menu height
+     */
+    getMenuHeight(): number;
+  }
+}
